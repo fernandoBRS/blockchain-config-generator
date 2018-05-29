6408676a1e096b44b3b402305bafdf1fc3a0a023
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-05-29 01:12:42 +0000

    Initial project structure

---
+++ WorkflowConfigWeb/WorkflowConfigWeb/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WorkflowConfigWeb.Models;
+
+namespace WorkflowConfigWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new HomeViewModel
+            {
+               Platforms = GetPlatforms()
+            };
+
+            return View(model);
+        }
+
+        private IEnumerable<BlockchainPlatform> GetPlatforms()
+        {
+            return new List<BlockchainPlatform>
+            {
+                new BlockchainPlatform
+                {
+                    Id = 0,
+                    Title = "Ethereum"
+                },
+                new BlockchainPlatform
+                {
+                    Id = 1,
+                    Title = "Hyperledger Fabric"
+                },
+                new BlockchainPlatform
+                {
+                    Id = 2,
+                    Title = "Corda"
+                }
+            };
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WorkflowConfigWeb/WorkflowConfigWeb/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿
+// hide all div options
+function hideOnLoad() {
+    $('#ethereum').hide();
+    $('#hyperledger').hide();
+    $('#corda').hide();
+}
+
+function handlePlatformSelection() {
+    $('#platforms').change(function () {
+        var selectedIndex = $(this).val();
+
+        switch (selectedIndex) {
+            case '0':
+                $('#ethereum').show();
+                $('#hyperledger').hide();
+                $('#corda').hide();
+                break;
+            case '1':
+                $('#ethereum').hide();
+                $('#hyperledger').show();
+                $('#corda').hide();
+                break;
+            case '2':
+                $('#ethereum').hide();
+                $('#hyperledger').hide();
+                $('#corda').show();
+                break;
+            default:
+                hideOnLoad();
+                break;
+        }
+    });
+}
+
+$(document).ready(function () {
+    hideOnLoad();
+    handlePlatformSelection();
+    
+
+    //Select item action
+    $("select").select2({ dropdownCssClass: 'dropdown-inverse' });
+});
