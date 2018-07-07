8d1517f03db5d6216fb0c9e44f42daa2fd377fc4
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-07-07 21:55:04 +0000

    Initial structure for Contract Manager and Configuration Manager

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const fileManager = require('./lib/file-manager');
+var ContractManager = require('./lib/contract-manager');
+var ConfigManager = require('./lib/config-manager');
 
+// clear();
 // console.log(
 //     chalk.green(
 //         figlet.textSync('BlockGen', { horizontalLayout: 'full' })
 //     )
 // );
 
-if (!fileManager.existsFileWithExtension('.sol')) {
+var contractManager = new ContractManager('.sol');
+
+if (!contractManager.existsAnyFile()) {
     console.log(chalk.red('No solidity file found in this directory!'));
     process.exit();
-}
+} 
+
+var contracts = contractManager.getContent();
+var configManager = new ConfigManager(contracts);
+
+configManager.createConfigFiles();
