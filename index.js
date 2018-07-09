15c863ea3f7ad6f552848f4d53a3ff2948f4e29a
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-07-09 16:19:18 +0000

    Files path as parameter for Contract and Config Manager

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const figlet = require('figlet');
 var ContractManager = require('./lib/contract-manager');
 var ConfigManager = require('./lib/config-manager');
 
-// clear();
-// console.log(
-//     chalk.green(
-//         figlet.textSync('BlockGen', { horizontalLayout: 'full' })
-//     )
-// );
-
-var contractManager = new ContractManager('.sol');
+var path = process.cwd();
+var contractManager = new ContractManager(path, '.sol');
 
 if (!contractManager.existsAnyFile()) {
     console.log(chalk.red('No solidity file found in this directory!'));
@@ -21,7 +15,7 @@ if (!contractManager.existsAnyFile()) {
 console.log(chalk.green('Creating config files...'));
 
 var contracts = contractManager.getContent();
-var configManager = new ConfigManager(contracts);
+var configManager = new ConfigManager(path, contracts);
 
 configManager.createConfigFiles();
 
